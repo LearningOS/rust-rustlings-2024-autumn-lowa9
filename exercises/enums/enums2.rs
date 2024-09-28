07c1ac0be99fdacd6e38e638f9f228140326670c
lowa9 <suxiaokai34@gmail.com>
2024-09-28 22:11:09 +0800

    update 9 exercises

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -3,11 +3,15 @@
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 #[derive(Debug)]
 enum Message {
     // TODO: define the different variants used below
+    Move { x: i32, y: i32 },
+    Echo(String),
+    ChangeColor(i32, i32, i32),
+    Quit,
 }
 
 impl Message {
@@ -21,7 +25,7 @@ fn main() {
         Message::Move { x: 10, y: 30 },
         Message::Echo(String::from("hello world")),
         Message::ChangeColor(200, 255, 255),
-        Message::Quit,
+        Message::Quit
     ];
 
     for message in &messages {
